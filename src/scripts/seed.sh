93dadfe9e04dacc2773f661405c2e5d9c6377dce
Bruno de Araujo Alves <me@baraus.dev>
2023-04-20 18:32:11 -0300

    Update seeds

--- src/scripts/seed.sh
+++ src/scripts/seed.sh
@@ -1,3 +1,7 @@
+# Rode o container emprega_web primeiro
+# Rode os scripts com o comando abaixo
+# docker exec -ti emprega_web bash -c "..."
+
 python manage.py seed_candidatos 1 --formacao 1~5 --experiencia 1~5 --curso 1~5 --idioma 1~5
 python manage.py seed_candidatos 1 --formacao_input fixtures/formacoes_academicas.json --experiencia_input fixtures/experiencia_profissional.json --curso_input fixtures/cursos_especializacoes.json --idioma_input fixtures/idiomas.json
 python manage.py seed_empregadores 1 --vagas 1~5
