431ff3bfb62ff60cf8860f9372d68d74f0d9394b
Bruno de Araújo Alves <me@baraus.dev>
2023-05-02 08:33:49 -0300

    Update

--- src/core/settings.py
+++ src/core/settings.py
@@ -21,7 +21,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # environ.Env.read_env(os.path.join(BASE_DIR, "../.env"))
-environ.Env.read_env(os.path.join(BASE_DIR, "../.env.local"))
+# environ.Env.read_env(os.path.join(BASE_DIR, "../.env.local"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
