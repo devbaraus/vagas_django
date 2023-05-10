d2383069620ebe3c0730a8522516fd63b69ac919
Bruno de Araújo Alves <me@baraus.dev>
2023-05-10 10:49:59 -0300

    Update confirmação de email

--- src/emprega/views.py
+++ src/emprega/views.py
@@ -667,7 +667,7 @@ class EmailVerificationView(APIView):
 
     @transaction.atomic
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
+        serializer = self.serializer_class(data=request.data, context={"request": request})
         serializer.is_valid(raise_exception=True)
 
         token = serializer.validated_data['token']
